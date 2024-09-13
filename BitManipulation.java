c7421f3755c7f2c4ec3579c9706fca05cd8de324
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2024-09-13 22:13:03 +0530

    added xorQueries in BitManipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -60,17 +60,54 @@ public class BitManipulation {
         return ans;
     }
 
+    /*
+     * Input: arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+     * Output: [2,7,14,8]
+     * Explanation:
+     * The binary representation of the elements in the array are:
+     * 1 = 0001
+     * 3 = 0011
+     * 4 = 0100
+     * 8 = 1000
+     * The XOR values for queries are:
+     * [0,1] = 1 xor 3 = 2
+     * [1,2] = 3 xor 4 = 7
+     * [0,3] = 1 xor 3 xor 4 xor 8 = 14
+     * [3,3] = 8
+     */
+    public static int[] xorQueries(int[] arr, int[][] queries) {
+        int xorArray[] = new int[arr.length + 1];
+
+        for (int i = 1; i < arr.length + 1; i++)
+            xorArray[i] = arr[i - 1] ^ xorArray[i - 1];
+
+        int[] ans = new int[queries.length];
+        int k = 0;
+
+        for (int[] i : queries)
+            ans[k++] = xorArray[i[1] + 1] ^ xorArray[i[0]];
+
+        return ans;
+
+    }
+
     public static void main(String[] args) {
-        int n = bitwiseComplement(5);
-        System.out.println(n);
-        getBit(5);
-        setBit(5);
-        clearBit(5);
+        // int n = bitwiseComplement(5);
+        // System.out.println(n);
+        // getBit(5);
+        // setBit(5);
+        // clearBit(5);
         // update bit also a bit manuplation which have two ways 1 to0 and 0 to 1
         // for 0 to 1 setbit operation method only
         // for 1 to 0 clear operationonly.
-        int a[] = { 1, 1, 2, 2, 4, 5, 5 };
-        findUniqueNoinarray(a);
-        System.out.println(minBitFlips(10, 7)); // 3 flips need to change the 10 to 7
+        // int a[] = { 1, 1, 2, 2, 4, 5, 5 };
+        // findUniqueNoinarray(a);
+        // System.out.println(minBitFlips(10, 7)); // 3 flips need to change the 10 to 7
+        int arr[] = { 1, 3, 4, 8 };
+        int queries[][] = { { 0, 1 }, { 1, 2 }, { 0, 3 }, { 3, 3 } };
+        int ans[] = xorQueries(arr, queries);
+        for (int i : ans)
+            System.out.print(i);
+
     }
 }
